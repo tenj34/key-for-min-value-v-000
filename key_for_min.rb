f65b2cd122bdc1c12ebee2f10cfee285c5a9d44f
Juan Tenezaca <tenj34@gmail.com>
2018-07-26 16:52:44 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,14 @@
 
 def key_for_min_value(name_hash)
 
+  smallest_key = nil
+  smallest_value = nil
+
+  name_hash.each do |name, num|
+    if smallest_value == nil || num < smallest_value
+      smallest_value = num
+      smallest_key = name
+    end
+  end
+  smallest_key
 end
