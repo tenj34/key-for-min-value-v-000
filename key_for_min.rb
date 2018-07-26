6d8c1c9fe1f1c5d86088659c4c94fd5dc22ac9aa
Juan Tenezaca <tenj34@gmail.com>
2018-07-26 16:48:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,9 @@ def key_for_min_value(name_hash)
   smallest_value = ""
 
   name_hash.each do |name, num|
+    if smallest_value == "" || num < smallest_value
+      smallest_value = num
+      smallest_key = name
 
 
   end
